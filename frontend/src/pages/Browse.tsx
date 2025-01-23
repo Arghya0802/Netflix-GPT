f6f1843fb83ffaff2dd0964eb3ac48b7cc62dc5d
Arghya0802 <chatterjeearghya02@gmail.com>
2025-01-23 19:03:27 +0530

    Header fixed

--- frontend/src/pages/Browse.tsx
+++ frontend/src/pages/Browse.tsx
@@ -70,12 +70,12 @@ export const Browse = () => {
 
   return (
     <div className="min-h-screen w-full">
-      <div className="flex items-center  md:h-20 md:justify-between w-full absolute z-30 top-0  md:py-2">
-        <div className="w-1/3 md:w-fit -mt-2 md:mt-2">
-          <Header height={400} width={400} isOpaque={!shouldShowGPT} />
+      <div className="flex items-center flex-col md:flex-row md:h-20 md:justify-between w-full absolute z-30 top-0  md:py-2">
+        <div className="w-full md:w-fit -mt-2 md:mt-2 flex justify-center">
+          <Header height={1000} width={1000} isOpaque={!shouldShowGPT} />
         </div>
 
-        <div className="md:px-2 flex md:gap-4 items-center gap-2 justify-center md:w-fit ml-2 w-full">
+        <div className="md:px-2 flex md:gap-4 items-center gap-5 justify-center md:w-fit ml-2 w-full">
           <LanguageDropDown
             onChange={(e) => {
               dispatch(changeUserLanguage(e.target.value));
